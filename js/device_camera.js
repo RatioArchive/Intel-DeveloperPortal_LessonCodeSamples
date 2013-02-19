9020d84c8e86d1a9f52afb32935336c0438f4cf2
Adam Argyle <atom@argyleink.com>
2013-02-19 13:51:47 -0800

    bug fixes

--- js/device_camera.js
+++ js/device_camera.js
@@ -1,7 +1,7 @@
 $(function() {
 
   var getDataBtn        = $('#getCameraData'),
-      video             = $('#cameraVideo'),
+      video             = document.getElementById('cameraVideo'),
       canvas            = document.getElementById('cameraCanvas'),
       ctx               = canvas.getContext('2d'),
       localMediaStream  = null;
