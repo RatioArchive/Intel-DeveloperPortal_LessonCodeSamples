851cf4d53c2d4c806370b7901e96c56f7bc2e7e0
Adam Argyle <atom@argyleink.com>
2013-03-22 08:49:35 -0700

    centering image element

--- Lesson 10/app.js
+++ Lesson 10/app.js
@@ -3,24 +3,27 @@ document.addEventListener('DOMContentLoaded', function() {
 	var paper
 	  , image
 	  , transformerOptions
-	  , transformer;
+	  , transformer
+	  , ww = window.innerWidth
+	  , wh = window.innerHeight;
 
 	// Creates canvas 320 × 200 at 10, 50
 	paper = Raphael(
 		0, // x
 		0, // y
-		window.innerWidth, // width
-		window.innerHeight // height
+		ww, // width
+		wh // height
 	);
 
 	// Creates image at x = 50, y = 40, with radius 10
 	image = paper.image(
 		'logo.svg',
-		153,
-		161,
-		400,
-		200
+		ww / 4,
+		wh / 4,
+		ww / 2,
+		wh / 2
 	);
+	image.attr({cursor:'pointer'});
 
 	// here's the setting that comic director uses on it's svg canvas items
 	transformerOptions = {
